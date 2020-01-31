567c98ab2aebeb21e3c454b1f4125206b2bcd6b0
Erick <erickluques@gmail.com>
2020-01-31 01:54:46 -0200

    Index method in ExperienceController.js

--- backend/src/controllers/ExperienceController.js
+++ backend/src/controllers/ExperienceController.js
@@ -1,5 +1,10 @@
 const Experience = require('../models/Experience');
 
 module.exports = {
-    
+    async index(req, res) {
+
+        const experiences = Experience.find({});
+
+        return res.json(experiences);
+    }
 }
