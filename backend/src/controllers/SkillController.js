b967bc124cb323d8492839868b112ab9804c597b
Erick <erickluques@gmail.com>
2020-01-31 00:52:29 -0200

    Store method in SkillController.js

--- backend/src/controllers/SkillController.js
+++ backend/src/controllers/SkillController.js
@@ -2,6 +2,19 @@ const Skill = require('../models/Skill');
 
 module.exports = {
     async index(req, res) {
+        const skills = await Skill.find({});
 
+        return res.json(skills);
+    },
+
+    async store(req, res) {
+        const { name, level } = req.body;
+
+        const skill = await Skill.create({
+            name,
+            level,
+        })
+
+        return res.json(skill);
     }
 }
