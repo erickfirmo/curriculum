8c919bf6beff34dac497032a5b28a59e252120bc
Erick <erickluques@gmail.com>
2020-01-31 01:26:22 -0200

    /**/

--- backend/src/controllers/CurseController.js
+++ backend/src/controllers/CurseController.js
@@ -3,6 +3,7 @@ const Curse = require('../models/Curse');
 module.exports = {
     async index(req, res) {
         
+        const curses = await Curse.find({});
 
         return res.json(curses);
     }
