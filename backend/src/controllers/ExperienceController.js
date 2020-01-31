dd67e866305b9212bf623117d6c1c1c38c50dfc9
Erick <erickluques@gmail.com>
2020-01-31 01:57:45 -0200

    Store method in ExperienceController.js

--- backend/src/controllers/ExperienceController.js
+++ backend/src/controllers/ExperienceController.js
@@ -6,5 +6,20 @@ module.exports = {
         const experiences = Experience.find({});
 
         return res.json(experiences);
+    },
+
+    async store(req, res) {
+
+        const { company, office, activities, start_date, end_date } = req.body;
+
+        experience = await Experience.create({
+            company,
+            office,
+            activities,
+            start_date,
+            end_date
+        });
+
+        return res.json(experience);
     }
 }
