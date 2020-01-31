b6cffefc2c3b78e1795987ed1f2614cb0b20613f
Erick <erickluques@gmail.com>
2020-01-31 01:16:41 -0200

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,4 +6,14 @@ const routes = express.Router();
 
 routes.get('/', SiteController.index);
 
+routes.get('/skills', SkillController.index);
+
+routes.post('/skills/store', SkillController.store);
+
+routes.post('/skills/update', SkillController.update);
+
+routes.get('/skills/:skill_id', SkillController.show);
+
+routes.post('/skills/delete/', SkillController.delete);
+
 module.exports = routes;
