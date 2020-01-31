5349d40afcaa586796ce146102411a2387b5a892
Erick <erickluques@gmail.com>
2020-01-31 01:30:39 -0200

    Store method in CurseController.js

--- backend/src/controllers/CurseController.js
+++ backend/src/controllers/CurseController.js
@@ -6,5 +6,19 @@ module.exports = {
         const curses = await Curse.find({});
 
         return res.json(curses);
+    },
+
+    async store(req, res) {
+        
+        const { name, hours, school, yeard } = req.body;
+
+        const curse = await Curse.create({
+            name,
+            hours,
+            school,
+            yeard
+        })
+
+        return res.json(curse);
     }
 }
