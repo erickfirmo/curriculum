072adee9a75ba28f156f9a87016da390b1fba792
Erick <erickluques@gmail.com>
2020-01-31 00:29:49 -0200

    Update SiteController.js

--- backend/src/controllers/SiteController.js
+++ backend/src/controllers/SiteController.js
@@ -1,15 +1,17 @@
 const Skill = require('../models/Skill');
 const Curse = require('../models/Curse');
-
+const Experience = require('../models/Experience');
 
 module.exports = {
     async index(req, res) {
         const skill = await Skill.find({});
         const curse = await Curse.find({});
+        const experience = await Experience.find({});
 
         return res.json({
             skill: skill,
             curse: curse,
+            experience: experience
         });
     }
 }
