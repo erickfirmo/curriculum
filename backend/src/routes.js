dfb882bd2cf1096edd30a37e3ff86e8eebc2f313
Erick <erickluques@gmail.com>
2020-01-30 22:32:55 -0200

    Update route.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const SiteController = require('./controllers/SiteController');
+
 const routes = express.Router();
+
+routes.get('/', SiteController.index);
 
 module.exports = routes;
