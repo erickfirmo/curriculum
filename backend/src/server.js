013aa9e8024eb4a8cef8105d76ef59c706a43bf2
Erick <erickluques@gmail.com>
2020-02-04 20:22:21 -0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://erickfirmo:psw@cluster0-nssil.mongodb.net/cv?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
