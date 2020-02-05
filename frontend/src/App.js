79b8e5e82faaa8bd594818ddb6ae4e43dee93e01
Erick <erickluques@gmail.com>
2020-02-05 19:34:04 -0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,8 @@ import './css/magnific-popup.css';
 import './css/style.css';
 import './css/colors/red.css';
 
-const Index1 = React.lazy(() => import('./pages/index-1'));
+const Index = React.lazy(() => import('./pages/index'));
+{/*const Index1 = React.lazy(() => import('./pages/index-1'));
 const Index2 = React.lazy(() => import('./pages/index-2'));
 const Index3 = React.lazy(() => import('./pages/index-3'));
 const Index4 = React.lazy(() => import('./pages/index-4'));
@@ -19,7 +20,7 @@ const Index5 = React.lazy(() => import('./pages/index-5'));
 const Index6 = React.lazy(() => import('./pages/index-6'));
 const Index7 = React.lazy(() => import('./pages/index-7'));
 const Index8 = React.lazy(() => import('./pages/index-8'));
-const Index9 = React.lazy(() => import('./pages/index-9'));
+const Index9 = React.lazy(() => import('./pages/index-9'));*/}
 
 const Login = React.lazy(() => import('./pages/Auth/login'));
 const Register = React.lazy(() => import('./pages/Auth/register'));
@@ -35,7 +36,8 @@ class App extends Component {
         <Router>
         <React.Suspense fallback={<div>&nbsp;</div>}>
           <Switch>
-              <Route path="/index1" component={Index1} />   
+              {/*
+              <Route path="/index1" component={Index1} /> 
               <Route path="/index2" component={Index2} />   
               <Route path="/index3" component={Index3} />   
               <Route path="/index4" component={Index4} />   
@@ -47,9 +49,12 @@ class App extends Component {
               
               <Route path="/login" component={Login} />  
               <Route path="/register" component={Register} />  
-              <Route path="/password-forget" component={ForgetPassword} /> 
+              
 
-              <Route path="/" component={Index1} /> 
+              <Route path="/password-forget" component={ForgetPassword} /> 
+              */}
+
+              <Route path="/" component={Index} /> 
 
            </Switch>
            </React.Suspense>
