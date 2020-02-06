c13cabcfdc48247aa605f10ae21a2e12a8366817
Erick <erickluques@gmail.com>
2020-02-06 12:26:15 -0200

    Update capa.js

--- frontend/src/pages/Shared/capa.js
+++ frontend/src/pages/Shared/capa.js
@@ -16,12 +16,13 @@ class Capa extends Component {
                 <section className="section capa p-0">
                     <div className="container">
                         <div className="row">
-                            <div className="col-12 col-sm-12 col-md-4 col-lg-4 text-center">
-                                <img src="https://avatars3.githubusercontent.com/u/34639603?s=460&v=4" alt="Foto de Perfil" className="img-fluid profile top-40"/>
-                            </div>
-                            <div className="col-12 col-sm-12 col-md-8 col-lg-8 d-flex">
+                            <div className="col-12 d-flex">
                                 <div className="my-auto top-40">
+                                    <img src="https://avatars3.githubusercontent.com/u/34639603?s=460&v=4" alt="Foto de Perfil" className="img-fluid profile"/>
+                                </div>
+                                <div className="my-auto info top-40">
                                     <h1>Érick Firmo</h1>
+                                    <h2 className="mx-auto">Desenvolvedor Full Stack</h2>
                                     <p className="home-desc mx-auto">Desenvolvedor Full Stack</p>
                                 </div>
                             </div>
