ef19f027258d914de8a5a7317b1175d02cbca486
Erick <erickluques@gmail.com>
2020-02-06 11:27:03 -0200

    Update capa.js

--- frontend/src/pages/Shared/capa.js
+++ frontend/src/pages/Shared/capa.js
@@ -14,16 +14,15 @@ class Capa extends Component {
         return (
             <React.Fragment>
                 <section className="section capa p-0">
-                    <div className="bg-overlay"></div>
                     <div className="container">
                         <div className="row">
                             <div className="col-12 col-sm-12 col-md-4 col-lg-4 text-center">
-                                <img src="https://avatars3.githubusercontent.com/u/34639603?s=460&v=4" alt="Foto de Perfil" className="img-fluid profile"/>
+                                <img src="https://avatars3.githubusercontent.com/u/34639603?s=460&v=4" alt="Foto de Perfil" className="img-fluid profile top-40"/>
                             </div>
-                            <div className="col-12 col-sm-12 col-md-8 col-lg-8 text-center d-flex">
-                                <div className="m-auto">
-                                    <h1 className="text-white">Érick Firmo</h1>
-                                    <p className="padding-t-15 home-desc mx-auto">Desenvolvedor Full Stack</p>
+                            <div className="col-12 col-sm-12 col-md-8 col-lg-8 d-flex">
+                                <div className="my-auto top-40">
+                                    <h1>Érick Firmo</h1>
+                                    <p className="home-desc mx-auto">Desenvolvedor Full Stack</p>
                                 </div>
                             </div>
                         </div>
