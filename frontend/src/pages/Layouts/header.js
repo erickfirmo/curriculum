1167980243d0d9e7712e031a3eec5b6753d8c227
Erick <erickluques@gmail.com>
2020-02-08 13:35:17 -0200

    Update header.js

--- frontend/src/pages/Layouts/header.js
+++ frontend/src/pages/Layouts/header.js
@@ -30,23 +30,19 @@ class Header extends Component {
                     <nav className="navbar navbar-expand-lg fixed-top navbar-custom sticky" id="nav-bar">
                         <div className="container">
                             <Link className="navbar-brand logo text-uppercase" to="/">
-                                <i className="mdi mdi-alien"></i>Hiric   </Link>
+                                Érick Firmo</Link>
 
                             <button className="navbar-toggler" onClick={this.toggleMenu} type="button">
                                 <i className="mdi mdi-menu"></i>
                             </button>
                             <div className={this.state.isOpen ? "collapse navbar-collapse show" : "collapse navbar-collapse"} style={{ display : this.state.isOpen ? "inline-grid" : "" }} id="navbarCollapse">
 
-                               
-                                        
                                     <ul className={this.state.isOpen ? "navbar-nav navbar-left" : "navbar-nav ml-auto navbar-left"} id="mySidenav">
                                         <li className="nav-item active"><a href="#home" className="nav-link">Home</a></li>
-                                        <li className="nav-item"><a href="#features" className="nav-link">Features</a></li>
-                                        <li className="nav-item"><a href="#services" className="nav-link">Services</a></li>
-                                        <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
-                                        <li className="nav-item"><a href="#pricing" className="nav-link">Pricing</a> </li>
-                                        <li className="nav-item"><a href="#blog" className="nav-link">Blog</a></li>
-                                        <li className="nav-item"><a href="#contact" className="nav-link">Contact</a> </li>
+                                        <li className="nav-item"><a href="#portfolio" className="nav-link">Portfólio</a></li>
+                                        <li className="nav-item"><a href="#habilidades" className="nav-link">Habilidades</a></li>
+                                        <li className="nav-item"><a href="#experiencia" className="nav-link">Experiência</a> </li>
+                                        <li className="nav-item"><a href="#contact" className="nav-link">Contato</a> </li>
                                     </ul>
 
                                 <div className={this.state.isOpen ? "nav-button" : "nav-button ml-auto"}>
