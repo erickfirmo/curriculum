89e06dff78e8919b3333e5e7d5a1014c82d9393a
erickfirmo <erickluque@gmail.com>
2020-02-12 19:58:10 -0300

    Update capa.js

--- frontend/src/pages/Shared/capa.js
+++ frontend/src/pages/Shared/capa.js
@@ -14,15 +14,15 @@ class Capa extends Component {
         return (
             <React.Fragment>
                 <section className="section capa p-0">
-                    <div className="container">
+                    <div className="container-fluid">
                         <div className="row">
-                            <div className="col-sm-12 col-md-5 d-flex justify-content-center">
+                            <div className="col-sm-12 col-md-4 d-flex justify-content-center">
                                 <div className="my-auto">
                                     <img src="https://avatars3.githubusercontent.com/u/34639603?s=460&v=4" alt="Foto de Perfil" className="img-fluid profile"/>
                                 </div>
                             </div>
-                            <div className="col-sm-12 col-md-7 d-flex justify-content-center">
-                                <div className="my-auto top-40 justify-content-end">
+                            <div className="col-sm-12 col-md-8 d-flex justify-content-center">
+                                <div className="my-auto justify-content-end">
                                     <h1>Érick Firmo</h1>
                                     <h2 className="mx-auto">Desenvolvedor Full Stack</h2>
                                     <p className="home-desc mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididu ut labore et dolore magna aliqua.</p>
